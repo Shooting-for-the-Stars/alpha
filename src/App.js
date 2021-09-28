88ae6739efca6e394fadcca4ced4f6a9f8ff741d
Joakim Sjöstedt <joakim.sjostedt@hm.com>
2021-09-28 10:52:17 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MONKI</h1>
+
+        <a href="http://styleid.com:3000/?brand=monki">
+          Login to with style id
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
